97f4af1de42302c7c862773954fc29286882d7e3
gcoda <gcoding@gmail.com>
2018-12-18 12:43:15 +0200

    add map and forward links to pages

--- clientPlugin.js
+++ clientPlugin.js
@@ -3,9 +3,23 @@ import backLinks from '@dynamic/backlinks'
 export default ({ Vue }) => {
   Vue.mixin({
     computed: {
+      $backLinksMap() {
+        return backLinks
+      },
       $backLinks() {
         return backLinks[this.$page.regularPath]
       },
-    }
+      $pageLinks() {
+        const pages = this.$site.pages.filter(
+          sitePage =>
+            this.$page.pageLinks
+              .map(x => x.path.replace(/\//g, ''))
+              .filter(
+                x => x === sitePage.path.replace(/\//g, '').replace('.html', '')
+              ).length > 0
+        )
+        return pages.filter(x => x.path.length > 1)
+      },
+    },
   })
 }
